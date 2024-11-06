b66f5025dc6ffc40fd2e0461b23c02e7b6d52e7f
Lucas Santos <lucassnts963@gmail>
2024-11-06 20:49:15 -0300

    chore: configurando setup do projeto

---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div>Está tudo rodando!</div>;
+}
---
+++ infra/database.js
@@ -0,0 +1,55 @@
+import { Client } from "pg";
+
+async function query(objectQuery) {
+    let client;
+
+    try {
+        client = await getNewClient();
+        const result = await client.query(objectQuery);
+        return result;
+    } catch (error) {
+        console.error("Database query error:", error);
+        throw error; // Re-throw the error so it can be handled by the caller
+    } finally {
+        if (client) {
+            await client.end();
+        }
+    }
+}
+
+async function getNewClient() {
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+        ssl: getSSLconfig(),
+    });
+
+    try {
+        await client.connect();
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        throw error;
+    }
+
+    return client;
+}
+
+function getSSLconfig() {
+    return true;
+    if (process.env.POSTGRES_CA) {
+        return {
+            ca: process.env.POSTGRES_CA,
+        };
+    }
+
+    // Enforce SSL in production for security
+    return process.env.NODE_ENV === "production" ? true : false;
+}
+
+export default {
+    getNewClient,
+    query,
+};
