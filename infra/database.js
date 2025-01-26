1469de77f20074b7afc986455d1368489fa3d2e2
Lucas Santos <lucassnts963@gmail>
2025-01-26 11:25:23 -0300

    adicionando prisma

--- infra/database.js
+++ infra/database.js
@@ -1,55 +1,18 @@
-import { Client } from "pg";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 async function query(objectQuery) {
-    let client;
-
-    try {
-        client = await getNewClient();
-        const result = await client.query(objectQuery);
-        return result;
-    } catch (error) {
-        console.error("Database query error:", error);
-        throw error; // Re-throw the error so it can be handled by the caller
-    } finally {
-        if (client) {
-            await client.end();
-        }
-    }
-}
-
-async function getNewClient() {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        ssl: getSSLconfig(),
-    });
-
-    try {
-        await client.connect();
-    } catch (error) {
-        console.error("Failed to connect to the database:", error);
-        throw error;
-    }
-
-    return client;
-}
-
-function getSSLconfig() {
-    return true;
-    if (process.env.POSTGRES_CA) {
-        return {
-            ca: process.env.POSTGRES_CA,
-        };
-    }
-
-    // Enforce SSL in production for security
-    return process.env.NODE_ENV === "production" ? true : false;
+  try {
+    const result = await prisma.$executeRaw`${objectQuery}`;
+    return result;
+  } catch (error) {
+    console.error("Database query error:", error);
+    throw error; // Re-throw the error so it can be handled by the caller
+  }
 }
 
 export default {
-    getNewClient,
-    query,
+  query,
+  prisma,
 };
